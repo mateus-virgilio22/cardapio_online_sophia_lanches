5c9c19b0197df2d744a6a4345a1bb3516f6dbc47
Mateus Virgílio <virgilionr15@gmail.com>
2023-10-31 20:35:07 -0300

    Substituindo imagens

--- js/dados.js
+++ js/dados.js
@@ -2,112 +2,112 @@ var MENU = {
     "cuscuz": [
         {
             "id": "best-of-texas-bbq-combo-serves-16",
-            "img": "./img/cardapio/churrasco/best-of-texas-bbq-combo-serves-14.7ae90266335e539c67e77fed14b43029.jpg",
+            "img": "./img/cardapio/cuscuz/cuscuz.jpg",
             "name": "Tradicional",
             "dsc": "Manteiga",
             "price": 7.00
         },
         {
             "id": "best-of-texas-bbq-combo-serves-19",
-            "img": "./img/cardapio/churrasco/best-of-texas-bbq-combo-serves-14.7ae90266335e539c67e77fed14b43029.jpg",
+            "img": "./img/cardapio/cuscuz/cuscuz.jpg",
             "name": "Tradicional",
             "dsc": "Leite",
             "price": 7.00
         },
         {
             "id": "ribs-brisket-and-burnt-ends",
-            "img": "./img/cardapio/churrasco/joes-kc-ribs-brisket-and-burnt-ends.6710e994980e485e6441b794717ad6fb.jpg",
+            "img": "./img/cardapio/cuscuz/cuscuz.jpg",
             "name": "Penosa I",
             "dsc": "Frango",
             "price": 7.00
         },
         {
             "id": "005-kings-carolina-oink-sampler",
-            "img": "./img/cardapio/churrasco/carolina-bbq-oink-sampler.1340b5a10cedc238cb2280306dd1d5a5.jpg",
+            "img": "./img/cardapio/cuscuz/cuscuz.jpg",
             "name": " Bacurin Picante I",
             "dsc": "Calabresa",
             "price": 7.00
         },
         {
             "id": "texas-monthlys-1-bbq-brisket",
-            "img": "./img/cardapio/churrasco/texas-monthlys-1-bbq-brisket.1006a061be7acae03992e420fbca995a.jpg",
+            "img": "./img/cardapio/cuscuz/cuscuz.jpg",
             "name": "Nordestina I",
             "dsc": "Carne de sol",
             "price": 8.00
         },
         {
             "id": "whole-brisket-texas-bbq-sauce",
-            "img": "./img/cardapio/churrasco/whole-brisket-texas-barbecue-bbq-sauce.e07ee4818b20ed43d217bf67fedd41ce.jpg",
+            "img": "./img/cardapio/cuscuz/cuscuz.jpg",
             "name": "Penosa II",
             "dsc": "Frango e queijo mussarela",
             "price": 9.00
         },
         {
             "id": "whole-texas-smoked-brisket",
-            "img": "./img/cardapio/churrasco/whole-texas-smoked-brisket.a5558a25381e271408e197936e7985d8.jpg",
+            "img": "./img/cardapio/cuscuz/cuscuz.jpg",
             "name": "Bacurin Picante II",
             "dsc": "Calabresa e queijo mussarela",
             "price": 9.00
         },
         {
             "id": "mini-trinity-bbq-combo-brisket-ribs-and-links",
-            "img": "./img/cardapio/churrasco/mini-trinity-bbq-combo-brisket-ribs-and-links.245582f593bf64b23b57dfca2be18cfd.jpg",
+            "img": "./img/cardapio/cuscuz/cuscuz.jpg",
             "name": "Nordestina II",
             "dsc": "Carne de sol e queijo mussarela",
             "price": 9.00
         },
         {
             "id": "235203-blue-smoke-baby-back-ribs-backyard-barbecue-chicken-combo",
-            "img": "./img/cardapio/churrasco/blue-smoke-baby-back-ribs-backyard-barbecue-chicken-combo.a95a3e632ae324f719738a2a5c1dff6e.jpg",
+            "img": "./img/cardapio/cuscuz/cuscuz.jpg",
             "name": "Penosa Chic",
             "dsc": "Frango, queijo mussarela e requeijão cremoso",
             "price": 10.00
         },
         {
             "id": "006-kings-meat-lovers-special",
-            "img": "./img/cardapio/churrasco/bbq-meat-lovers-special-for-10.36ca670fda4bfa783c2ea9165e068d26.jpg",
+            "img": "./img/cardapio/cuscuz/cuscuz.jpg",
             "name": "Bacurin Picante III",
             "dsc": "Calabresa, queijo mussarela e requeijão cremoso",
             "price": 10.00
         },
         {
             "id": "the-big-ugly-bbq-dinner-for-6",
-            "img": "./img/cardapio/churrasco/the-big-ugly-bbq-dinner-for-6.2dfae7818811adddce85cc1a910881a0.jpg",
+            "img": "./img/cardapio/cuscuz/cuscuz.jpg",
             "name": "Nordestina III",
             "dsc": "Carne de sol, queijo mussarela e requeijão cremoso",
             "price": 10.00
         },
         {
             "id": "17796-mighty-quinns-bbq-sampler-pack",
-            "img": "./img/cardapio/churrasco/mighty-quinns-bbq-sampler-pack.1bfe4a0665edc565756f5241bf25840e.jpg",
+            "img": "./img/cardapio/cuscuz/cuscuz.jpg",
             "name": "Avechada",
             "dsc": "Ovo, queijo coalho e requeijão",
             "price": 10.00
         },
         {
             "id": "post-oak-smoked-half-brisket",
-            "img": "./img/cardapio/churrasco/post-oak-smoked-usda-prime-half-brisket.6f17178dde7d806670bcc73ff11762b3.jpg",
+            "img": "./img/cardapio/cuscuz/cuscuz.jpg",
             "name": "Lampião",
             "dsc": "Bacon, queijo coalho e requeijão cremoso",
             "price": 13.00
         },
         {
             "id": "best-of-texas-bbq-combo-serves-14",
-            "img": "./img/cardapio/churrasco/best-of-texas-bbq-combo-serves-14.7ae90266335e539c67e77fed14b43029.jpg",
+            "img": "./img/cardapio/cuscuz/cuscuz.jpg",
             "name": "Vixe Maria",
             "dsc": "Carne de sol, queijo coalho e requeijão cremoso",
             "price": 14.00
         },
         {
             "id": "best-of-texas-bbq-combo-serves-15",
-            "img": "./img/cardapio/churrasco/best-of-texas-bbq-combo-serves-14.7ae90266335e539c67e77fed14b43029.jpg",
+            "img": "./img/cardapio/cuscuz/cuscuz.jpg",
             "name": "Amostrado",
             "dsc": "Carne de sol, calabresa, bacon e requeijão",
             "price": 15.00
         },
         {
             "id": "best-of-texas-bbq-combo-serves-18",
-            "img": "./img/cardapio/churrasco/best-of-texas-bbq-combo-serves-14.7ae90266335e539c67e77fed14b43029.jpg",
+            "img": "./img/cardapio/cuscuz/queijo.jpg",
             "name": "Queijo coalho",
             "dsc": "",
             "price": 1.00
@@ -116,42 +116,42 @@ var MENU = {
     "pao_na_chapa": [
         {
             "id": "the-gramercy-tavern-burger-4-pack",
-            "img": "./img/cardapio/burguers/Gramercy-Tavern-Burger-and-Kielbasa-Kit-6.4.21-72ppi-1x1-15.jpg",
+            "img": "./img/cardapio/pao/pao.jpg",
             "name": "Misto quente",
             "dsc": "Pão francês, mussarela e presunto",
             "price": 4.00
         },
         {
             "id": "shake-shack-shackburger-8-pack",
-            "img": "./img/cardapio/burguers/shake-shack-shackburger-8-pack.973a5e26836ea86d7e86a327becea2b0.jpg",
+            "img": "./img/cardapio/pao/pao.jpg",
             "name": "Tipo 1",
             "dsc": "Pão francês, frango e mussarela",
             "price": 6.00
         },
         {
             "id": "gotts-cheeseburger-kit-for-4",
-            "img": "./img/cardapio/burguers/gotts-complete-cheeseburger-kit-for-4.7bdc74104b193427b3fe6eae39e05b5e.jpg",
+            "img": "./img/cardapio/pao/pao.jpg",
             "name": "Tipo 2",
             "dsc": "Pão francês, carne de sol e mussarela",
             "price": 8.00
         },
         {
             "id": "le-big-matt-kit-for-6",
-            "img": "./img/cardapio/burguers/le-big-matt-kit-for-6.1ddae6e382bb3218eeb0fd5247de115a.jpg",
+            "img": "./img/cardapio/pao/pao.jpg",
             "name": "Tipo 3",
             "dsc": "Pão francês, frango, ovo e mussarela",
             "price": 8.00
         },
         {
             "id": "shake-shack-shackburger-16-pack",
-            "img": "./img/cardapio/burguers/shake-shack-shackburger-16-pack.316f8b09144db65931ea29e34869287a.jpg",
+            "img": "./img/cardapio/pao/pao.jpg",
             "name": "Tipo 4",
             "dsc": "Pão francês, carne de sol, ovo e mussarela",
             "price": 9.00
         },
         {
             "id": "21-usda-prime-burgers-pack-of-18-8oz-each",
-            "img": "./img/cardapio/burguers/usda-prime-burgers-pack-of-18-8oz-each.274c67f15aa1c0b210dbf51801706670.jpg",
+            "img": "./img/cardapio/cuscuz/queijo.jpg",
             "name": "Queijo coalho",
             "dsc": "",
             "price": 1.00
@@ -160,49 +160,49 @@ var MENU = {
     "sucos": [
         {
             "id": "15259-german-chocolate-killer-brownie-tin-pack",
-            "img": "./img/cardapio/sobremesas/german-chocolate-killer-brownie-tin-pack.5ebc34160f28767a9d94c4da2e04c4b9.jpg",
+            "img": "./img/cardapio/suco/goiaba.jpg",
             "name": "Goiaba",
             "dsc": "",
             "price": 4.00
         },
         {
             "id": "jacques-world-famous-chocolate-chip-cookies",
-            "img": "./img/cardapio/sobremesas/jacques-world-famous-chocolate-chip-cookies-6-pack.2217a14c443602493bba88aa9335319a.jpg",
+            "img": "./img/cardapio/suco/manga.jpg",
             "name": "Manga",
             "dsc": "",
             "price": 4.00
         },
         {
             "id": "luigis-original-cannoli-pie",
-            "img": "./img/cardapio/sobremesas/original-cannoli-pie.4cb5b9ba82f57b69b90765fd9f07aa1a.jpg",
+            "img": "./img/cardapio/suco/acerola.jpg",
             "name": "Acerola",
             "dsc": "",
             "price": 4.00
         },
         {
             "id": "sea-salted-caramel-swirl-cheesecake",
-            "img": "./img/cardapio/sobremesas/sea-salted-caramel-swirl-cheesecake.e2825335433fb7a272a5d77649a6849e.jpg",
+            "img": "./img/cardapio/suco/caja.webp",
             "name": "Cajá",
             "dsc": "",
             "price": 4.00
         },
         {
             "id": "brooklyn-blackout-cookie-brownie-combo-pack-2-tins",
-            "img": "./img/cardapio/sobremesas/brooklyn-blackout-cookie-brownie-combo-pack-2-tins.d8805325baf6b23b4f01d119dc4531a7.jpg",
+            "img": "./img/cardapio/suco/caju.jpg",
             "name": "Caju",
             "dsc": "",
             "price": 4.00
         },
         {
             "id": "best-seller-cupcake-dozen",
-            "img": "./img/cardapio/sobremesas/best-seller-cupcake-dozen.f93b21993f6a2da11c975d45b9b0d08f.jpg",
+            "img": "./img/cardapio/suco/abacaxi.jpg",
             "name": "Abacaxi",
             "dsc": "",
             "price": 5.00
         },
         {
             "id": "choose-your-own-ice-cream-donuts-6-pack",
-            "img": "./img/cardapio/sobremesas/choose-your-own-ice-cream-donuts-6-pack.24d0b44765a7c54237fcd7ea9d9d8093.jpg",
+            "img": "./img/cardapio/suco/graviola.jpeg",
             "name": "Graviola",
             "dsc": "",
             "price": 5.00
@@ -211,28 +211,28 @@ var MENU = {
     "brownie": [
         {
             "id": "hong-kong-boba-tea-kit-for-6",
-            "img": "./img/cardapio/bebidas/hong-kong-boba-tea-kit-for-6.63841de36d8e5edfafa13023fc303285.jpg",
+            "img": "./img/cardapio/brownie/brownie.jpg",
             "name": "Tradicional",
             "dsc": "",
             "price": 5.00
         },
         {
             "id": "guys-caliente-margaritas-for-12",
-            "img": "./img/cardapio/bebidas/guys-caliente-margaritas-for-12.ca8c6bc06b8f1039549385ffcebc749d.jpg",
+            "img": "./img/cardapio/brownie/brownie.jpg",
             "name": "Recheado",
             "dsc": "Ninho",
             "price": 8.00
         },
         {
             "id": "woodford-reserve-mint-julep-syrup",
-            "img": "./img/cardapio/bebidas/woodford-reserve-mint-julep-syrup.ef523ac7cbae5f4aba6b058207f490d2.jpg",
+            "img": "./img/cardapio/brownie/brownie.jpg",
             "name": "Recheado",
             "dsc": "Brigadeiro",
             "price": 8.00
         },
         {
             "id": "new-orleans-hurricane-mix",
-            "img": "./img/cardapio/bebidas/new-orleans-hurricane-mix.4613584fc65cb0787024dd24d2a8f4b3.jpg",
+            "img": "./img/cardapio/brownie/brownie.jpg",
             "name": "Recheado",
             "dsc": "Cacau 50%",
             "price": 8.00
